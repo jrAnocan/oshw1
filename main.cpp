3be96ae155a8da33643b7d15b1401cd940033f05
jrAnocan <52035643+jrAnocan@users.noreply.github.com>
2023-03-29 16:08:30 +0300

    main.cpp
    first

---
+++ main.cpp
@@ -0,0 +1,192 @@
+#include "logging.h"
+#include "message.h"
+#include<iostream>
+#include <stdlib.h>
+#include <string>
+#include<vector>
+#include <sys/wait.h> 
+#include <poll.h>
+#include <cstring>
+
+using namespace std;
+
+#define READ_END 0
+#define WRITE_END 1
+
+
+typedef struct bomber
+{
+    coordinate c;
+    vector<string> args;
+} bomber;
+
+
+void getObstacles(vector<obsd>*& obstacles, int obstacle_count, char* argv[])
+{
+    for(int i=0;i<obstacles->size();i++)
+    {
+        obsd o;
+        o.position.x = stoi(argv[5+3*i]);
+        o.position.y = stoi(argv[6+3*i]);
+        o.remaining_durability = stoi(argv[7+3*i]);
+        obstacles->push_back(o);
+    }
+
+}
+string bomberArguments(vector<bomber> bombers, int n)
+{
+    string res;
+    for(int i=0;i<bombers[n].args.size();i++)
+    {
+        res += bombers[n].args[i];
+        res += " ";
+    }
+    return res;
+}
+
+void closeOtherPipes(int pipes[][2], int n, int bomber_count)
+{
+    int i;
+    for(i=0;i<bomber_count;i++)
+    {
+        if(i!=n)
+        {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    vector<string> allArgs(argv, argv+argc); // first is ./bgame. Start reading from second.
+
+
+    int width, height, obstacle_count, bomber_count, status;
+    width = stoi(allArgs[1]); height = stoi(allArgs[2]); obstacle_count = stoi(allArgs[3]); bomber_count = stoi(allArgs[4]);
+    
+    vector<obsd>* obstacles = new vector<obsd>;
+    
+    
+    getObstacles(obstacles,obstacle_count,argv);
+
+    int pipes[bomber_count][2];
+    struct pollfd polls[bomber_count];
+    
+    int pid_table[bomber_count] = { };
+
+    
+    vector<bomber> bombers ;
+            
+    int offset = 0;
+    
+    for(int i=0; i<bomber_count ; i++)
+    {
+        bomber b;
+        int ind = 5+3*obstacle_count+offset;
+        int arg_count = stoi(argv[ind+2]);
+        b.c.x = stoi(argv[ind]); b.c.y = stoi(argv[ind+1]);
+        int j;
+        for(j=0; j<arg_count; j++)
+        {
+            b.args.push_back( argv[ind+3+j] );
+        }
+        
+        b.args.push_back("\0");
+        offset += 2+arg_count+1;
+        bombers.push_back(b);
+ 
+    }
+   
+    
+
+    //cout << bombers[1].args[3]<<endl;
+   
+    
+    for(int i=0; i<bomber_count;i++) // create pipes
+    {
+        PIPE(pipes[i]);
+        
+    }
+    
+    
+   
+   
+    for(int i=0;i<bomber_count;i++)
+    {
+        int pid = fork();
+        pid_table[i]=pid;
+
+        if(pid)
+        {
+            string msg = bomberArguments(bombers, i);
+            
+            write(pipes[i][WRITE_END], &(bombers[i].args), sizeof(struct bomber));
+           
+            
+            
+        }
+        else
+        {
+            vector<string>* args;
+            dup2(pipes[i][WRITE_END],fileno(stdin));
+            
+
+            
+            read(pipes[i][WRITE_END], args, sizeof(struct bomber));
+            dup2(pipes[i][READ_END],fileno(stdout));
+            closeOtherPipes(pipes,i,bomber_count);
+        
+
+            char* inp[(*args).size()];
+            for(int i=0;i<(*args).size();i++)
+            {
+                char* c = new char[(*args)[i].length() + 1];
+
+                // Copy contents
+                copy((*args)[i].begin(), (*args)[i].end(), c);
+                inp[i] = c;
+            }
+            //cout<<"my pid is:"<<getpid()<<endl;
+            //execv(inp[0],inp);
+            int p = getpid();
+            cout<<p<<endl;
+            return 0;
+            //execl("/bin/ls","ls",NULL); // exec the children
+        }
+    }
+    /*
+    for(int i=0;i<bomber_count;i++)
+    {
+        cout<<pid_table[i]<<" ";
+    }
+    */
+   for(int i=0; i<bomber_count;i++) // create pipes
+    {
+        
+        memset(&(polls[i]), 0, sizeof(polls[i]));
+        polls[i].fd = pipes[i][1];
+        polls[i].events = POLLIN;
+
+    }
+    char buf[1024];
+                    read(pipes[0][READ_END], buf, 1024);
+                    cout<<buf<<endl;
+                    
+                    read(pipes[1][READ_END], buf, 1024);
+                    cout<<buf<<endl;
+    
+    
+    
+
+
+    
+  
+    for(int i=0;i<bomber_count;i++)
+    {
+        waitpid(pid_table[i], &status, 0);
+    }
+
+    return 0;
+
+}
