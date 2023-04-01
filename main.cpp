8bfaf8f825ca5548461802db101ef94cbe7f3ea4
jrAnocan <52035643+jrAnocan@users.noreply.github.com>
2023-04-01 15:13:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,8 @@ typedef struct bomber
 {
     coordinate c;
     vector<string> args;
+    bool marked;
+    bool die_message_recieved;
 } bomber;
 
 typedef struct bomb
@@ -27,8 +29,10 @@ typedef struct bomb
     pid_t pid;
     int fd[2];
     struct pollfd poll;
+    int radius;
 } bomb;
 
+int bomber_count;
 
 void getObstacles(vector<obsd>*& obstacles, int obstacle_count, vector<vector<string>>& lines)
 {
@@ -59,7 +63,7 @@ void closeOtherPipes(int pipes[][2], int n, int bomber_count)
 
 int bomberThere(vector<bomber>& bombers, coordinate check_coordinate)
 {
-    int res=0;
+    int res=-1;
     for(int i=0;i<bombers.size();i++)
     {
         if(bombers[i].c.x == check_coordinate.x && bombers[i].c.y == check_coordinate.y)
@@ -70,9 +74,23 @@ int bomberThere(vector<bomber>& bombers, coordinate check_coordinate)
     return res;
 }
 
+int bombThere(vector<bomb>& bombs, coordinate c)
+{
+    int res=-1;
+    
+    for(int i=0;i<bombs.size();i++)
+    {
+        if(bombs[i].c.x == c.x && bombs[i].c.y == c.y)
+        {
+            return i;
+        }
+    }
+    return res;
+}
+
 int obstacleThere(vector<obsd>*& obstacles, coordinate check_coordinate)
 {
-    int res=0;
+    int res=-1;
     for(int i=0;i<(*obstacles).size();i++)
     {
         if((*obstacles)[i].position.x == check_coordinate.x && (*obstacles)[i].position.y == check_coordinate.y)
@@ -84,35 +102,69 @@ int obstacleThere(vector<obsd>*& obstacles, coordinate check_coordinate)
 }
 
 
-vector<vector<int>> indexesOfNearObjects(vector<bomber>& bombers, vector<obsd>*& obstacles, int n, int width, int height)
+vector<vector<int>> indexesOfNearObjects(vector<bomber>& bombers, vector<bomb>& bombs, vector<obsd>*& obstacles, int n, int width, int height)
 {
-    // BOMBER + OBSTACLE   
+    // BOMBER + BOMB + OBSTACLE   
+
+    
+
+
     vector<vector<int>> res;
+    for(int i=0;i<3;i++)
+    {
+        res.push_back(vector<int>());
+    }
+
     coordinate initial_coordinate = bombers[n].c;
+    /*
+    for(int i=0;i<bombs.size();i++)
+    {
+        cout<<"current coordinate: "<<initial_coordinate.x<<","<<initial_coordinate.y<<endl;
+        cout<<"bomb "<<i<<" "<<bombs[i].c.x<<","<<bombs[i].c.y<<endl;
+    }
+    */
+
     for(int i=-3;i<=3;i++)
     {
         int offset = abs(i);
-        res.push_back(vector<int>());
+        
         for(int j=-3+offset;j<=3-offset;j++)
         {
             
-            if(i==0 && j==0) continue; //exclude self
-            if(i>width-1 || j > height-1) continue; // exclude out of borders
+            
+            
+            
             coordinate check_coordinate;
 
             check_coordinate.x = initial_coordinate.x + j; // first check the horizontals
             check_coordinate.y = initial_coordinate.y + i; 
 
             int bomber_index = bomberThere(bombers, check_coordinate);
+            int bomb_index = bombThere(bombs, check_coordinate);
             int obstacle_index = obstacleThere(obstacles,check_coordinate);
-            if(bomber_index)
+            if((bomber_index>=0) && i!=0 && j!=0)
             {
                 res[0].push_back(bomber_index);
             }
             
-            else if(obstacle_index)
+             if((bomb_index>=0))
             {
-                res[1].push_back(obstacle_index);
+                res[1].push_back(bomb_index);
+            }
+            /*
+            if(i==0 && j==0)
+            {
+                for(int i=0;i<bombs.size();i++)
+                {
+                    cout<<"current coordinate: "<<check_coordinate.x<<","<<check_coordinate.y<<endl;
+                    cout<<"bomb "<<i<<" "<<bombs[i].c.x<<","<<bombs[i].c.y<<endl;
+                    
+                }
+            }
+            */
+             if((obstacle_index>=0))
+            {
+                res[2].push_back(obstacle_index);
             }
         }
     }
@@ -136,18 +188,149 @@ bool newCoordinateAvailable(coordinate current, coordinate target, int width, in
     return false;
 }
 
-int bombThere(vector<bomb>& bombs, coordinate c)
+int getBombIndex(vector<bomb>& bombs, int pid)
 {
+    
     for(int i=0;i<bombs.size();i++)
     {
-        if(bombs[i].c.x == c.x && bombs[i].c.y == c.y)
+        if(pid == bombs[i].pid)
         {
-            return 1;
+            return i;
         }
     }
-    return 0;
+    return -1;
 }
 
+bool obstacleBetween(vector<obsd>*& obstacles, coordinate bomber_position, coordinate explosion_origin)
+{
+    bool same_line_x;
+    bool same_line_y;
+    if(bomber_position.y == explosion_origin.y) {same_line_x = true;}
+    if(bomber_position.x == explosion_origin.x) {same_line_y = true;}    
+    if((same_line_x && same_line_y) || (!same_line_x && !same_line_y)) // on the bomb || on different lines
+    {
+        return false;
+    }
+    else if(same_line_x && !same_line_y)
+    {
+        for(int i=0;i<(*obstacles).size();i++)
+        {
+            coordinate obstacle_coordinate = (*obstacles)[i].position;
+            int min_x = min(bomber_position.x, explosion_origin.x); int max_x = max(bomber_position.x, explosion_origin.x);
+            if( (obstacle_coordinate.y == bomber_position.y) && 
+                (min_x < obstacle_coordinate.x) &&
+                (obstacle_coordinate.x < max_x)  )
+            {
+                return true;
+            }
+               
+        }
+    }
+    else if(same_line_y && !same_line_x)
+    {
+        for(int i=0;i<(*obstacles).size();i++)
+        {
+            coordinate obstacle_coordinate = (*obstacles)[i].position;
+            int min_y = min(bomber_position.y, explosion_origin.y); int max_y = max(bomber_position.y, explosion_origin.y);
+            if( (obstacle_coordinate.x == bomber_position.x) && 
+                (min_y < obstacle_coordinate.y) &&
+                (obstacle_coordinate.y < max_y)  )
+            {
+                return true;
+            }
+               
+        }
+    }
+
+    return false;
+}
+
+
+bool caughtInExplosion(vector<obsd>*& obstacles, coordinate object_position, coordinate explosion_origin, int radius, object_type type)
+{
+    bool res = false;
+    switch (type)
+    {
+    case OBSTACLE:
+    {
+        if(object_position.x == explosion_origin.x && abs((int) object_position.y - (int) explosion_origin.y)<=radius)
+        {
+            res = true;
+        }
+        else if(object_position.y == explosion_origin.y && abs((int) object_position.x - (int) explosion_origin.x)<=radius)
+        {
+            res = true;
+        }
+        break;
+    }
+    case BOMBER:
+    {
+        if(obstacleBetween(obstacles, object_position, explosion_origin))
+        {
+            res = false;
+        }
+        else
+        {
+            if(object_position.x == explosion_origin.x && abs((int) object_position.y - (int) explosion_origin.y)<=radius)
+            {
+                res = true;
+            }
+            else if(object_position.y == explosion_origin.y && abs((int) object_position.x - (int) explosion_origin.x)<=radius)
+            {
+                res = true;
+            }
+        }
+
+        break;
+    }   
+    default:
+        break;
+    }
+    return res;
+}
+
+void decreaseDurabilityOfObstacles(vector<obsd>*& obstacles, coordinate c, int radius)
+{
+    for(int i=0;i<obstacles->size();i++)
+    {
+        if(caughtInExplosion(obstacles, (*obstacles)[i].position, c, radius, OBSTACLE))
+        {
+            int durability = (*obstacles)[i].remaining_durability;
+            if( durability== -1)
+            {
+                continue;
+            }
+            else if(durability == 1)
+            {
+               (*obstacles).erase((*obstacles).begin() + i); 
+            }
+            else
+            {
+                (*obstacles)[i].remaining_durability--;
+            }
+        }
+    }
+}
+
+void informMarkedBomber(vector<bomber>& bombers, int pipes[][2], int pid_table[], int n )
+{
+    om* message_out = new om;
+    
+    omp* message_out_print = new omp;
+    message_out->type = BOMBER_DIE;
+    
+    message_out_print->pid = pid_table[n];
+    message_out_print->m = message_out;
+
+    send_message(pipes[n][WRITE_END], message_out);
+    print_output(NULL,message_out_print,NULL,NULL);
+                        
+    close(pipes[n][WRITE_END]);
+    bomber_count--;
+    delete message_out;   
+    delete message_out_print;
+
+}
 void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, int n, int pipes[][2], int pid_table[]
                         , vector<obsd>*& obstacles, int width, int height)
 {
@@ -172,6 +355,12 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
     }
     case BOMBER_MOVE:
     {
+        if(bombers[n].marked )
+        {
+           informMarkedBomber(bombers, pipes, pid_table, n);
+           return;
+        }
+
         om* message_out = new om;
         omd* message_out_data = new omd;
         omp* message_out_print = new omp;
@@ -204,7 +393,12 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
     }
     case BOMBER_PLANT:
     {
-    
+        if(bombers[n].marked )
+        {
+           informMarkedBomber(bombers, pipes, pid_table, n);
+           return;
+        }
+
         coordinate bomber_location ;
         int bomber_pid = im->pid;
         int bomber_index;
@@ -217,7 +411,7 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
         }    
                   //----------------->didnt check if pid exists in pid table. Assume it would.
         bomber_location = bombers[bomber_index].c;
-        int is_bomb_there = bombThere(bombs, bomber_location);
+        int is_bomb_there = bombThere(bombs, bomber_location)+1; ///-----------------------------------------> IMPORTANt. bombThere returns -1 if false
         if(!is_bomb_there) // No bomb ---> plant
         {
             int fd[2];
@@ -228,7 +422,7 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
             
             b.c = bomber_location;
             b.fd[0] = fd[0]; b.fd[1] = fd[1];
-            
+            b.radius = im->m->data.bomb_info.radius;
             
             int pid = fork();
             
@@ -311,28 +505,37 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
     
     case BOMB_EXPLODE:
     {
-        /*
-        int bomb_index;
-        int status;
-        for(bomb_index=0;bomb_index<bombs.size();bomb_index++)
+        
+        int bomb_index = getBombIndex(bombs,im->pid);    
+        if(bomb_index==-1)
         {
-            if(im->pid == bombs[bomb_index].pid)
-            {
-                break;
-            }
+            perror("bomb_index is -1");
+            return;
         }
+        
+        if(caughtInExplosion(obstacles, bombers[n].c,bombs[bomb_index].c,bombs[bomb_index].radius,BOMBER))
+        {
+            bombers[n].marked = true;
 
-        waitpid(bombs[bomb_index].pid,&status,0);
-        */
+        }
+        decreaseDurabilityOfObstacles(obstacles, bombs[bomb_index].c,bombs[bomb_index].radius );
         break;
     }
 
     case BOMBER_SEE:
     {
-        vector<vector<int>> near_objects = indexesOfNearObjects(bombers, obstacles, n, width, height);
+        if(bombers[n].marked )
+        {
+           informMarkedBomber(bombers, pipes, pid_table, n);
+           return;
+        }
+
+        
+
+
+        vector<vector<int>> near_objects = indexesOfNearObjects(bombers, bombs, obstacles, n, width, height);
     
         int object_count = near_objects[0].size() + near_objects[1].size() + near_objects[2].size(); // BOMBER + BOMB + OBSTACLE  
-        
         od objects[object_count];  //objects in an array
         
         om* message_out = new om;
@@ -340,7 +543,10 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
         omp* message_out_print = new omp;
         
         int ind=0;
-        for(int i=0;i<2;i++)  // 2 types of objects --> 0 -> bomber, 1 -> obstacle
+        
+        
+        
+        for(int i=0;i<3;i++)  // 3 types of objects --> 0 -> bomber, 1->bomb 2 -> obstacle
         {
             for(int j=0;j<near_objects[i].size();j++) 
             {
@@ -354,7 +560,14 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
                     break;
                 }
                 
-                case 1: //obstacle
+                case 1: //bomb
+                {
+                    obj.type = BOMB;
+                    obj.position = bombs[near_objects[i][j]].c;
+                    break;
+                }   
+
+                case 2: //obstacle
                 {
                     obj.type = OBSTACLE;
                     obj.position = (*obstacles)[near_objects[i][j]].position;
@@ -395,19 +608,6 @@ void serveBomberMessage(imp*& im, vector<bomber>& bombers, vector<bomb>& bombs, 
     }
 }
 
-bool bombExploded(int pid, vector<int>& exploded_bomb_pids)
-{
-    for(int i=0;i<exploded_bomb_pids.size();i++)
-    {
-        if(pid == exploded_bomb_pids[i])
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-
 int main()
 {
     string l;
@@ -432,7 +632,7 @@ int main()
     }
   
     
-    int width, height, obstacle_count, bomber_count, status;
+    int width, height, obstacle_count, status;
     width = stoi(lines[0][0]); height = stoi(lines[0][1]); obstacle_count = stoi(lines[0][2]); bomber_count = stoi(lines[0][3]);
     
     vector<obsd>* obstacles = new vector<obsd>;
@@ -460,6 +660,8 @@ int main()
         b.c.x = stoi(lines[obstacle_count+1+i][0]); 
         b.c.y = stoi(lines[obstacle_count+1+i][1]); 
         b.args = lines[obstacle_count+2+i];
+        b.marked = false;
+        b.die_message_recieved = false;
         bombers.push_back(b);
     }
     
@@ -515,7 +717,7 @@ int main()
             bomb_polls[i] = bombs[i].poll;
         }
 
-        int ret_bomb = poll(bomb_polls,size,100);
+        int ret_bomb = poll(bomb_polls,size,0);
         
         
         if(ret_bomb)
@@ -551,7 +753,7 @@ int main()
         
         
 
-        int ret_bomber = poll(polls,bomber_count,10);
+        int ret_bomber = poll(polls,bomber_count,0);
         
         if(ret_bomber) // &&!ret_bomb
         {
@@ -562,6 +764,7 @@ int main()
                 if(polls[i].revents & POLLIN )
                 {
                     
+
                     im* m = new im;
                     imp* mp = new imp;
                     read_data(pipes[i][WRITE_END], m);
@@ -582,10 +785,25 @@ int main()
         
         sleep(0.001);  
     }
-  
+    
+    for(int i=0;i<bombers.size();i++)
+    {
+        
+        
+            close(pipes[i][WRITE_END]);
+        
+    }
+    for(int i=0;i<bombs.size();i++)
+    {
+        close(bombs[i].fd[WRITE_END]);
+    }
     for(int i=0;i<bomber_count;i++)
     {
-        waitpid(pid_table[i], &status, 0);
+        if(bombers[i].die_message_recieved)
+        {
+            waitpid(pid_table[i], &status, 0);
+        }
+        
     }
     
     return 0;
